125a0bc0bc27f7a3849920a0f9763ea91a77ff28
Sarthak Kumar Yadav <ysarthakyadav3737@gmail.com>
2026-06-15 23:42:29 +0530

    day07q1

---
+++ roataell.java
@@ -0,0 +1,57 @@
+
+// public class ListNode {
+//       int val;
+//       ListNode next;
+//       ListNode() {}
+//       ListNode(int val) { this.val = val; }
+//       ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+//   }
+
+
+public class roataell {
+    class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        
+        if (head == null || head.next == null || k == 0) {
+            return head;
+        }
+
+        
+        // Find length and tail
+        int length = 1;
+        ListNode tail = head;
+
+        while (tail.next != null) {
+            tail = tail.next;
+            length++;
+        }
+
+        // Handle k > length
+        k = k % length;
+
+        if (k == 0) {
+            return head;
+        }
+
+        // Make circular list
+        tail.next = head;
+
+        // Find new tail
+        int steps = length - k - 1;
+        ListNode newTail = head;
+
+        for (int i = 0; i < steps; i++) {
+            newTail = newTail.next;
+        }
+
+        // New head
+        ListNode newHead = newTail.next;
+
+        // Break the circle
+        newTail.next = null;
+
+        return newHead;
+    }
+}
+    
+}
